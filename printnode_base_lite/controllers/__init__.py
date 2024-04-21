47bd75ec002fb59d43c0e430a2fc3ee20b8ea405
mahmudamen <mahmudamen@gmil.com>
2024-04-22 01:52:28 +0200

    remove addons

--- printnode_base_lite/controllers/__init__.py
+++
@@ -1,4 +0,0 @@
-# Copyright 2023 VentorTech OU
-# See LICENSE file for full copyright and licensing details.
-
-from . import main
--- printnode_base_lite/tests/__init__.py
+++
@@ -1,2 +0,0 @@
-# Copyright 2023 VentorTech OU
-# License OPL-1.0 or later.
